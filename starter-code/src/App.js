06e29622be16e672ce4fabc2c5d694000c71eea8
Bianca <ing_biank@hotmail.com>
2019-04-29 22:25:08 +0200

    adds Navbar component in App

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar'
+
+
+class App extends React.Component {
+  render(){
+    const element = (
+      <div>
+        <Navbar />
+      </div>
+    );
+    return element
+  }
+}
+
+export default App;
