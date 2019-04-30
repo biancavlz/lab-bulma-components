22738bef70da50ecea2b8e52af6d99f3b5870c50
Bianca <ing_biank@hotmail.com>
2019-04-30 09:28:34 +0200

    some comments as refeneces

--- starter-code/src/FormField.js
+++ starter-code/src/FormField.js
@@ -1,6 +1,8 @@
 import React from 'react'
+// import React, {Component} from 'react'
 
 class FormField extends React.Component {
+  //class FormField extends Component { 
   render(){
     const formField = (
       <div>
@@ -16,5 +18,21 @@ class FormField extends React.Component {
     return formField;
   }
 }
+
+//or as a function instead of a class
+// const FormField = (props) => {
+//   const { label, type, placeholder } = props; => distraucturing equivalent to:
+      // const label = props.label
+      // const type = props.typel
+      // const placeholder = props.placeholder
+//   return (
+//     <div className="field">
+//       <label className="label">{label}</label>
+//       <div className="control">
+//         <input className="input" type={type} placeholder={placeholder} />
+//       </div>
+//     </div>
+//   )
+// }
 
 export default FormField;
